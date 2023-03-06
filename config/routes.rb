a54232e8feb500bd9d28a2cd41f512727782795a
Edward Fernando <edward.fernando@gojek.com>
2023-03-06 10:42:06 +0700

    add new routes to see sleep records

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       resources :users, only: [:create] do
+        get '/users/:id/sleep_records', to: "users#sleep_records", as: "sleep_records"
         post '/users/:id/follow', to: "users#follow", as: "follow_user"
         post '/users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"
 
